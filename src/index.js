adc23d55976027d98059707d084374b2d627faad
alldsaiw <milianalynx@gmail.com>
2022-06-30 22:42:12 +0300

    started developing to-do list

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const elem = <h2>Hello World</h2>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-elem
-);
+<App/>);

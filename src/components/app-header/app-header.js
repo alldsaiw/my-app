4d0e3a875acbe494fc69de22fa8245aaba44a1cc
alldsaiw <milianalynx@gmail.com>
2022-06-30 23:31:25 +0300

    smth not working

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import './app-header.css'
+
+const AppHeader = () => {
+    return (
+        <div className="app-header">
+            <h1>Что мы сделаем сегодня?</h1>
+        </div>
+    )
+}
+
+export default AppHeader;
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import AppHeader from '../app-header/app-header';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2 class='header-txt'>Hello World!</h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader/>
     </div>
   );
 }
